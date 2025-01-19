1d0c9d0f59b5d65dd4499d7577a5985e08d64e14
Kamal <kamaldeep734833@gmail.com>
2025-01-19 23:53:36 +0530

    add custum security bean

--- src/main/java/com/Jwt/security/Jwt/Security/Config/CustomConfig.java
+++ src/main/java/com/Jwt/security/Jwt/Security/Config/CustomConfig.java
@@ -1,10 +1,30 @@
 package com.Jwt.security.Jwt.Security.Config;
 
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class CustomConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors->cors.disable())
+                .sessionManagement(request->request
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request->request
+                        .requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
 }
